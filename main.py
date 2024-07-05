bb605c6288cb9e4c26348ffb454105ffad5e238e
PyEZIO <151058514+elimsi@users.noreply.github.com>
2024-07-05 14:20:00 +0100

    Add image_to_grid conversion logic

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ from Graph import Graph
 import matplotlib.pyplot as plt
 
 img = cv2.imread("lol.png")
+
+def image_to_grid(img):
+    is_road = (img[:, :, 0] > 100) & (img[:, :, 1] > 100) & (img[:, :, 2] > 100)
+    grid = np.zeros(img.shape[:2], dtype=np.uint8)
+    grid[is_road] = 255
+    return grid
+
+grid = image_to_grid(img)
+graph = Graph(grid)
